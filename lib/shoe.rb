11712dc4767b925f6fcf9d26701d1b35a3630259
Taylor Sieling <taylorasieling@gmail.com>
2020-09-03 01:47:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -10,5 +12,12 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand(brand)
+    @brand = brand
+    BRANDS << brand 
+  end
+  
+  
 
 end
