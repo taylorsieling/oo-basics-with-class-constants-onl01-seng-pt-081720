ad87b1e9ba90f11d57ba0355574769df33b463a4
Taylor Sieling <taylorasieling@gmail.com>
2020-09-03 01:52:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,21 +3,16 @@ class Shoe
   attr_reader :brand, :unique_brand
   
   BRANDS = []
-  UNIQUE_BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if 
     BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-  
-  def unique_brand(unique_brand)
-    @unique_brand = unique_brand
-    UNIQUE_BRANDS << unique_brand 
   end
   
 end
