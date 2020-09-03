4419215d04a21398f05d91a4dc6d4e054ddb37d5
Taylor Sieling <taylorasieling@gmail.com>
2020-09-03 01:48:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,10 +14,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand(brand)
-    @brand = brand
-    BRANDS << brand 
-  end
+  # def brand(brand)
+  #   @brand = brand
+  #   BRANDS << brand 
+  # end
   
   
 
